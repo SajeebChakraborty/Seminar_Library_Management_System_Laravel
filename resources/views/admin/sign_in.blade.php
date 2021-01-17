35e788d44998526b2b39e149e53f2fbc276cee44
Sajeeb Chakraborty <48250220+SajeebChakraborty@users.noreply.github.com>
2021-01-17 11:58:18 +0600

    Version 1.0

--- resources/views/admin/sign_in.blade.php
+++ resources/views/admin/sign_in.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Seminar Library Management System</title>
-    <link rel="stylesheet" href="{{ secure_asset('css/style4.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style4.css') }}">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css2?family=Hanalei+Fill&display=swap" rel="stylesheet">
